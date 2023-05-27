640ddec16d59714cc4cbeeb747345c4f671fecf2
lesskop <lesskop61@gmail.com>
2023-05-27 15:08:53 +0300

    Add sql solutions to problems #181, #183 and #577

---
+++ sql/577_employee_bonus.sql
@@ -0,0 +1,9 @@
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b ON e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus IS NULL;
+
+--SELECT name, bonus
+--FROM Employee
+--LEFT JOIN Bonus USING(empID)
+--WHERE COALESCE(bonus, 0) < 1000;
