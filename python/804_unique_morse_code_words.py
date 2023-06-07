ec5f5fd16b98eb88646a1fa5f09c546158e603fb
lesskop <lesskop61@gmail.com>
2023-06-07 21:28:28 +0300

    Update #804 python solution

--- python/804_unique_morse_code_words.py
+++ python/804_unique_morse_code_words.py
@@ -5,8 +5,8 @@ class Solution:
 
         char_to_code = {chr(i + 97): morse_codes[i] for i in range(26)}
 
-        transformations = []
+        transformations = set()
         for word in words:
-            transformations.append(''.join([char_to_code.get(char) for char in word]))
+            transformations.add(''.join([char_to_code.get(char) for char in word]))
 
-        return len(set(transformations))
+        return len(transformations)
