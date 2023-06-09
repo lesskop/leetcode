87d7c8d58638c72f48f5c708efd0338ee4f9f02d
lesskop <lesskop61@gmail.com>
2023-06-09 13:24:15 +0300

    Add 7 solutions for easy SQL problems

---
+++ sql/1581_customer_who_visited_but_did_not_make_any_transactions.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT
+    customer_id,
+    COUNT(visit_id) AS count_no_trans
+FROM
+    Visits
+LEFT JOIN
+    Transactions USING(visit_id)
+WHERE
+    transaction_id IS NULL
+GROUP BY
+    customer_id;
