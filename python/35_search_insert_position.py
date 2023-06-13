f401c507dae9993e96288c0a47d39872abea65ab
lesskop <lesskop61@gmail.com>
2023-06-13 11:04:05 +0300

    Add python binary search solution to #35 problem

--- python/35_search_insert_position.py
+++ python/35_search_insert_position.py
@@ -1,12 +1,31 @@
 class Solution:
     def searchInsert(self, nums: list[int], target: int) -> int:
-        try:
-            return nums.index(target)
-        except ValueError:
-            if target < min(nums):
-                return 0
-            elif target > max(nums):
-                return len(nums)
+        # # Trivial solution using `index()` method
+        # try:
+        #     return nums.index(target)
+        # except ValueError:
+        #     if target < min(nums):
+        #         return 0
+        #     elif target > max(nums):
+        #         return len(nums)
+        #     else:
+        #         nums.append(target)
+        #         return sorted(nums).index(target)
+
+        # Binary search solution
+        start = 0
+        end = len(nums) - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+
+            if target == nums[mid]:
+                return mid
+
+            if target > nums[mid]:
+                start = mid + 1
+
             else:
-                nums.append(target)
-                return sorted(nums).index(target)
+                end = mid - 1
+
+        return start
