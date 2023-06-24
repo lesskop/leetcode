ff5b262cf96af3782db1337d43e3318b51e84faa
lesskop <lesskop61@gmail.com>
2023-06-24 13:37:18 +0300

    Add sql solutions to problems #1731, #1789 and #1978

---
+++ sql/1731_the_number_of_employees_which_report_to_each_employee.sql
@@ -0,0 +1,15 @@
+SELECT
+    m.employee_id,
+    m.name,
+    COUNT(e.employee_id) AS reports_count,
+    ROUND(AVG(e.age)) AS average_age
+FROM
+    employees e
+JOIN
+    employees m
+    ON
+    e.reports_to = m.employee_id
+GROUP BY
+    employee_id
+ORDER BY
+    employee_id;
